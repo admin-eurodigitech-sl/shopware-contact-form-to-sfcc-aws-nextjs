d3673a3afb7e52cdbd370fbf02f72eb32566a9d2
admin-eurodigitech-sl <admin@eurodigitech.es>
2024-12-18 20:41:31 +0000

    feat: added calls

--- src/pages/api/webhook.js
+++ src/pages/api/webhook.js
@@ -1,14 +1,60 @@
-export default function handler(req, res) {
-    console.log("Hualla loco me han llamado");
-    console.log("=====req=====");
-    console.log(req);
+const md5 = require('md5');
 
-    console.log("=====req.method=====");
-    console.log(req.method);
-    
-    console.log("=====req.body=====")
-    console.log(req.body);
+export default async function handler(req, res) {
+    try {
+        //FUNCTION IS BEING CALLED
+        console.log("Hualla loco me han llamado");
 
 
-    res.status(200).json({ message: 'Hello I m a webhook!' })
+        //GET AUTHORIZATION TOKEN
+        const getTokenUrl = process.env.GET_TOKEN_ENDPOINT;
+        const grantType = process.env.GRANT_TYPE;
+        const clientId = process.env.CLIENT_ID;
+        const clientSecret = process.env.CLIENT_SECRET;
+        const getTokenHeaders = new Headers();
+
+        getTokenHeaders.append("Content-Type", "application/json");
+
+        const getTokenResponse = await fetch(getTokenUrl, {
+            method: "POST",
+            headers: getTokenHeaders,
+            body: JSON.stringify({
+                grant_type: grantType,
+                client_id: clientId,
+                client_secret: clientSecret,
+            }),
+        });
+
+        const { access_token } = await getTokenResponse.json();
+
+        //POST DATA
+
+        let postData = "" // TODO: Hanlde post data calculation
+
+        //POST CONTACT DATA INTO SFCC
+        const postEndpoint = process.env.POST_ENDPOINT;
+        const postContactInfoHeaders = new Headers();
+
+        getTokenHeaders.append("Content-Type", "application/json");
+        getTokenHeaders.append("Authorization", `Bearer ${access_token}`);
+
+        const postResponse = await fetch(postEndpoint, {
+            method: "POST",
+            headers: postContactInfoHeaders,
+            body: JSON.stringify([
+                {
+                    "keys": {
+                        "MemberId": md5(JSON.stringify(postData))
+                    },
+                    "values": postData
+                }
+            ])
+        })
+
+        const { status, body } = await postResponse.json();
+
+        res.status(status).json({ body })
+    } catch (e) {
+        return Error(e.message);
+    }
 }
