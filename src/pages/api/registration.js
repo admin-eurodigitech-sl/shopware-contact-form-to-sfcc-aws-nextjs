118037aaab1a4a31df473a55be738d139b6265c0
admin-eurodigitech-sl <admin@eurodigitech.es>
2024-12-27 19:11:42 +0000

    feat: added register

---
+++ src/pages/api/registration.js
@@ -0,0 +1,24 @@
+const md5 = require('md5');
+
+export default async function handler(req, res) {
+    try {
+        console.log("Register!");
+
+        const { shopId, shopUrl, secret } = req.body;
+
+
+        if (secret !== 'MySecretTest') {
+            return res.status(403).send('Invalid secret');
+        }
+
+        console.log('App registered for shop:', shopId);
+
+
+        console.log('Shop url is:', shopUrl);
+
+
+        res.status(200).json({ body: "Registration successful" });
+    } catch (e) {
+        return Error(e.message);
+    }
+}
