15bdf68b1392a25d292c71ee6a86d249dc984205
admin-eurodigitech-sl <admin@eurodigitech.es>
2024-12-27 19:36:00 +0000

    feat: added things

--- src/pages/api/registration.js
+++ src/pages/api/registration.js
@@ -3,20 +3,7 @@ const md5 = require('md5');
 export default async function handler(req, res) {
     try {
         console.log("Register called!");
-        const url = new URL(req.url);
-        console.log(url);
-
-        const searchParams = url.searchParams;
-
-        console.log(searchParams);
-
-
-        const shopId = searchParams.get("shopId");
-        const shopUrl = searchParams.get("shopUrl");
-
-        console.log(shopId);
-        console.log(shopUrl);
-
+        console.log(req.url);
 
         res.status(200).json({ body: "Registration successful" });
     } catch (e) {
